fcfa9ba208ce711ad28214f36fc8fb637ac27073
GildoJeremias <gildojeremias6@gmail.com>
2022-11-18 17:23:51 +0100

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let js = 'amazing';
-if (js === 'amazing') alert ("Gostei do programa");
-
-10+20+30;
 console.log(10+20+30);
+
+console.log('Jonas');
+console.log(89);
+
+let firstName = 'Jonas';
+console.log(js)
+console.log(js)
+console.log(js)
+
+let jonas_Matilda = "JM";
+let PI = 3.1415;
+
+console.log(PI);
+console.log(jonas_Matilda);
+
+let myFirstJob = "Vendedor de oléo de motor";
+let mySecondJob = "Vídeo Produtor";
+console.log("Meu primeiro Emprego: ",myFirstJob);
+console.log("Meu segundo emprego",mySecondJob);
